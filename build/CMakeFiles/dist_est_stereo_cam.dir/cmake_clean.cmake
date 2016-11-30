4668905bb26497ff7ce453ca2400d32bcba613fd
Winston Poh <wpo20@uclive.ac.nz>
2016-11-30 14:40:56 +1300

    Initial commit

---
+++ build/CMakeFiles/dist_est_stereo_cam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dist_est_stereo_cam.dir/main.cpp.o"
+  "dist_est_stereo_cam.pdb"
+  "dist_est_stereo_cam"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dist_est_stereo_cam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
